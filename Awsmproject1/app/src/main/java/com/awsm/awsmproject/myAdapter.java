3cddd6bdc63da5efce72ae8738f4e43ae3ab0c1f
Yashal Shakti <shaktiyashal@gmail.com>
2015-02-01 16:43:19 +0530

    OnClicks()

--- Awsmproject1/app/src/main/java/com/awsm/awsmproject/myAdapter.java
+++ Awsmproject1/app/src/main/java/com/awsm/awsmproject/myAdapter.java
@@ -3,9 +3,13 @@ package com.awsm.awsmproject;
 /**
  * Created by Yashal Shakti
  */
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -16,14 +20,16 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.io.InputStream;
 import java.util.List;
-
+import java.util.Locale;
 
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implements View.OnClickListener{
@@ -31,6 +37,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implem
     Context mContext;
     JSONArray brandStores;
 
+    int LENGTH;
     private static final String TAG_NAME="brandOutletName";
     private static final String TAG_RATING="ratingValue";
     private static final String TAG_NUMBER="phoneNumber";
@@ -38,6 +45,11 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implem
     private static final String TAG_LOCLAT="locationLat";
     private static final String TAG_LOCLONG="locationLong";
     private static final String TAG_IMAGE="imageUrl";
+    public String storeName;
+    public String callnumber;
+    public Location location;
+    public String onlineImages;
+    String loc[]={"",""};
 
 
     public MyAdapter(String myStores[], Context context,JSONArray jsonstores)
@@ -45,6 +57,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implem
         stores=myStores;
         mContext=context;
         brandStores=jsonstores;
+        LENGTH=jsonstores.length();
     }
 
 
@@ -59,30 +72,72 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implem
     public void onBindViewHolder(ViewHolder viewHolder, int i) {
 
         try {
-            viewHolder.storeName.setText(brandStores.getJSONObject(i).getString(TAG_NAME));
-        } catch (JSONException e) { e.printStackTrace(); }
-        try { String url =brandStores.getJSONObject(i).getString(TAG_IMAGE); Log.i("Image url",url);
+            storeName=brandStores.getJSONObject(i).getString(TAG_NAME);
+            viewHolder.storeName.setText(storeName);
+            String url =brandStores.getJSONObject(i).getString(TAG_IMAGE); Log.i("Image url",url);
             new DownloadImageTask(viewHolder.storeImage).execute(url);
-        } catch (JSONException e) { e.printStackTrace(); }
-        try {
             float rating=(float)((Integer.parseInt(brandStores.getJSONObject(i).getString(TAG_RATING)))/2.0);
             viewHolder.rating.setRating(rating);
-        } catch (JSONException e) {
+           callnumber=brandStores.getJSONObject(i).getString(TAG_NUMBER);
+          /*  location.setLatitude(Double.parseDouble(brandStores.getJSONObject(i).getString(TAG_LOCLAT)));
+            location.setLongitude(Double.parseDouble(brandStores.getJSONObject(i).getString(TAG_LOCLONG)));*/
+            loc[0]=brandStores.getJSONObject(i).getString(TAG_LOCLAT);
+            loc[1]=brandStores.getJSONObject(i).getString(TAG_LOCLONG);
+            onlineImages="https://www.google.com/search?site=imghp&tbm=isch&source=hp&biw=&bih=&q="+storeName.substring(0,storeName.indexOf(" "))+" store&btnG=Search+by+image";
+        }
+         catch (Exception e) {
             e.printStackTrace();
         }
 
         viewHolder.call.setOnClickListener(this);
+        viewHolder.map.setOnClickListener(this);
+         viewHolder.olImages.setOnClickListener(this);
 
         //viewHolder.storeImage.setImageResource(mContext.getResources().getIdentifier(stores[i].toLowerCase(), "drawable", mContext.getPackageName()));
     }
 
     @Override
     public int getItemCount() {
-        return stores.length;
+        return LENGTH;
     }
 
     @Override
     public void onClick(View v) {
+        Context oContext;
+        oContext= mContext;
+        switch(v.getId())
+        {
+            case R.id.call:
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", callnumber, null));
+                oContext.startActivity(intent);
+                break;
+            case R.id.map:
+                String uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?&daddr=%s,%s (%s)", loc[0], loc[1], storeName);
+                Intent mapintent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                mapintent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                try
+                {
+                    oContext.startActivity(mapintent);
+                }
+                catch(ActivityNotFoundException ex)
+                {
+                    try
+                    {
+                        Intent unrestrictedIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                        oContext.startActivity(unrestrictedIntent);
+                    }
+                    catch(ActivityNotFoundException innerEx)
+                    {
+                        Toast.makeText(oContext, "Please Install a MAPS Application", Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+            case R.id.images:
+                Uri uri2 = Uri.parse(onlineImages);
+                Intent Imagesintent = new Intent(Intent.ACTION_VIEW, uri2);
+                oContext.startActivity(Imagesintent);
+                break;
+        }
 
     }
 
@@ -93,7 +148,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implem
         public ImageView storeImage;
         public ImageButton call;
         public ImageButton map;
-        public ImageButton images;
+        public ImageButton olImages;
         public RatingBar rating;
 
         public ViewHolder(View itemView) {
@@ -102,7 +157,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implem
             storeName= (TextView)itemView.findViewById(R.id.storeName);
             call=(ImageButton)itemView.findViewById(R.id.call);
             map=(ImageButton)itemView.findViewById(R.id.map);
-            images=(ImageButton)itemView.findViewById(R.id.image);
+            olImages=(ImageButton)itemView.findViewById(R.id.images);
             rating=(RatingBar)itemView.findViewById(R.id.ratingBar);
         }
     }
